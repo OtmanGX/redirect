605b44adc52d7df22346a06bc70ea8b6c3c5d61d
otmangx <otmangx@gmail.com>
2021-02-04 18:13:43 +0100

    Allow all

--- redirect/settings.py
+++ redirect/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'liq^w6j7a=47b^%89)%b&^)!+u!z$$s0y1#p6ao0p7hpm$5^#%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
